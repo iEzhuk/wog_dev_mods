0347bed0e5ba1e7c25b554437164e75c64bc728e
Gollob <gollob701@gmail.com>
2015-04-29 14:56:15 +0300

    Update config.cpp

--- addons/wog3_rds_Static/config.cpp
+++ addons/wog3_rds_Static/config.cpp
@@ -5,12 +5,12 @@
 
 class CfgPatches
 {
-	class WOG_RDS_StaticW_RHSpatch
+	class wog3_rds_Static
 	{
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {"A3_Weapons_F","rhs_main","rhs_c_weapons"};
+		requiredAddons[] = {"RDS_StaticWeapons_Core","rhs_main","rhs_c_weapons"};
 		version = "1";
 	};
 };
@@ -19,55 +19,55 @@ class CfgMagazines
     class VehicleMagazine;
 	class RDS_6Rnd_TOW_HMMWV: VehicleMagazine
 	{
-		ammo = "rhs_ammo_TOW2_AT";
+		ammo = "rhs_ammo_TOW_AT";
 	};
-		class RDS_6Rnd_TOW2: VehicleMagazine
+	class RDS_6Rnd_TOW2: VehicleMagazine
 	{
-		ammo = "rhs_ammo_TOW2_AT";
+		ammo = "rhs_ammo_TOW_AT";
 	};
-		class RDS_6Rnd_AT13: VehicleMagazine
+	class RDS_6Rnd_AT13: VehicleMagazine
 	{
 
 		ammo="rhs_mag_9m113_3";
 
 	};
-		class RDS_29Rnd_30mm_AGS30: VehicleMagazine
+	class RDS_29Rnd_30mm_AGS30: VehicleMagazine
 	{
 
 		ammo="64Rnd_40mm_G_belt";
 
 	};
-		class RDS_150Rnd_127x107_DSHKM: VehicleMagazine
+	class RDS_150Rnd_127x107_DSHKM: VehicleMagazine
 	{
 
 		ammo = "rhs_ammo_127x108mm";
 
 	};
-			class RDS_150Rnd_127x108_KORD: VehicleMagazine
+	class RDS_150Rnd_127x108_KORD: VehicleMagazine
 	{
 
 		ammo = "rhs_ammo_127x108mm";
 
 	};
-				class RDS_40Rnd_23mm_AZP85: VehicleMagazine
+	class RDS_40Rnd_23mm_AZP85: VehicleMagazine
 	{
 
 		ammo = "RHS_ammo_23mm_AA";
 
 	};
-				class RDS_2000Rnd_23mm_AZP85: VehicleMagazine
+	class RDS_2000Rnd_23mm_AZP85: VehicleMagazine
 	{
 
 		ammo = "RHS_ammo_23mm_AA";
 
 	};
-				class RDS_Igla: VehicleMagazine
+	class RDS_Igla: VehicleMagazine
 	{
 
 		ammo = "rhs_ammo_9k38";
 
 	};
-					class RDS_2Rnd_Igla: VehicleMagazine
+	class RDS_2Rnd_Igla: VehicleMagazine
 	{
 
 		ammo = "rhs_ammo_9k38";

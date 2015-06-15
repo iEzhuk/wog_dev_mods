de3ef8620f7942c9e1fb10e0e371cc1d02d0df91
iEzhuk <a1dokuchaev@gmail.com>
2015-06-15 15:17:14 +0300

    Map fixs

---
+++ addons/wog3_mod/CfgMapFix.h
@@ -0,0 +1,141 @@
+class CfgWorlds
+{
+	class DefaultWorld;
+	class CAWorld: DefaultWorld
+	{
+		class Grid{};
+	};
+	class Chernarus: CAWorld
+	{
+		class Grid: Grid
+		{
+			offsetY = 15360;
+			class Zoom1
+			{
+				zoomMax = 0.075;
+				format = "XY";
+				formatX = "000";
+				formatY = "000";
+				stepX = 100;
+				stepY = -100;
+			};
+			class Zoom2
+			{
+				zoomMax = 1;
+				format = "XY";
+				formatX = "00";
+				formatY = "00";
+				stepX = 1000;
+				stepY = -1000;
+			};
+			class Zoom3
+			{
+				zoomMax = 1e+030;
+				format = "XY";
+				formatX = "0";
+				formatY = "0";
+				stepX = 10000;
+				stepY = 10000;
+			};
+		};
+	};
+	class FDF_Isle1_a: CAWorld
+	{
+		class Grid: Grid {
+			offsetY = 20480;
+			class Zoom1
+			{
+				zoomMax = 0.075;
+				format = "XY";
+				formatX = "000";
+				formatY = "000";
+				stepX = 100;
+				stepY = -100;
+			};
+			class Zoom2
+			{
+				zoomMax = 1;
+				format = "XY";
+				formatX = "00";
+				formatY = "00";
+				stepX = 1000;
+				stepY = -1000;
+			};
+			class Zoom3
+			{
+				zoomMax = 1e+030;
+				format = "XY";
+				formatX = "0";
+				formatY = "0";
+				stepX = 10000;
+				stepY = 10000;
+			};
+		};
+	};
+	class Takistan: CAWorld
+	{
+		class Grid: Grid
+		{
+			class Zoom1
+			{
+				zoomMax = 0.1;
+				format = "XY";
+				formatX = "000";
+				formatY = "000";
+				stepX = 100;
+				stepY = -100;
+			};
+			class Zoom2
+			{
+				zoomMax = 1;
+				format = "XY";
+				formatX = "00";
+				formatY = "00";
+				stepX = 1000;
+				stepY = -1000;
+			};
+			class Zoom3
+			{
+				zoomMax = 1e+030;
+				format = "XY";
+				formatX = "0";
+				formatY = "0";
+				stepX = 10000;
+				stepY = 10000;
+			};
+		};
+	};
+	class Zargabad: CAWorld
+	{
+		class Grid: Grid
+		{
+			class Zoom1
+			{
+				zoomMax = 0.18;
+				format = "XY";
+				formatX = "000";
+				formatY = "000";
+				stepX = 100;
+				stepY = -100;
+			};
+			class Zoom2
+			{
+				zoomMax = 1;
+				format = "XY";
+				formatX = "00";
+				formatY = "00";
+				stepX = 1000;
+				stepY = -1000;
+			};
+			class Zoom3
+			{
+				zoomMax = 1e+030;
+				format = "XY";
+				formatX = "0";
+				formatY = "0";
+				stepX = 10000;
+				stepY = 10000;
+			};
+		};
+	};
+};
